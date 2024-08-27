fdafa2da1d495b5e37c3700e61c75d24c6717d94
Saccharin01 <chousik01@gmail.com>
2024-08-28 03:49:52 +0900

    mongoose connect test

---
+++ skeleton/test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+
+dotenv.config()
+  
+  mongoose.connect(process.env.DATABASE)
+  .then(() => {
+      const userSchema = new mongoose.Schema({
+          _id : String,
+          src: String
+      });
+  
+      // User 모델 생성
+      const musicData = mongoose.model('staticLink', userSchema,'staticLink');
+  
+      // 사용자 데이터 불러오기
+      musicData.find({})
+          .then(users => {
+              console.log('모든 사용자 데이터:', users);
+              return users
+          })
+          .catch(error => {
+              console.error('데이터 조회 오류:', error);
+          });
+  })
+  .catch((error) => {
+      console.error('MongoDB 연결 오류:', error);
+  });
+
