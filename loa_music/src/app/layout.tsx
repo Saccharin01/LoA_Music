e34b99fe736770ed28dc948a9f600cd46ac2499b
Saccharin01 <chousik01@gmail.com>
2024-09-05 04:50:35 +0900

    backup

--- loa_music/src/app/layout.tsx
+++ loa_music/src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import DragElement from "./components/DragElement";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LoAmusic",
-  description: "LoskArk Theme music",
+  title: "LoA Music",
+  description: "Losk Ark Theme music",
 };
 
 export default function RootLayout({
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className="w-screen h-screen">
+        <div className="flex h-4/5 justify-between">
+          <main className="w-3/5 bg-indigo-300 m-5">playerComponent will be here</main>
+
+          <aside className="flex w-2/5 bg-emerald-200  mr-5 my-5 flex-col justify-between">
+            <div className="bg-orange-200 h-4/6">description here</div>
+            <div className="bg-orange-500 h-56">dropBox here</div>
+          </aside>
+        </div>
+
+        <footer className="h-1/5 bg-amber-700 mx-5">
+          <DragElement />
+        </footer>
+      </body>
     </html>
   );
 }
