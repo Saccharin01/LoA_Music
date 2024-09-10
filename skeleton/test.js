3afeef2a04f44c1553f3d94840b1ab3d665e0889
Saccharin01 <chousik01@gmail.com>
2024-09-10 21:59:47 +0900

    mongoose test

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,29 +1,26 @@
-const mongoose = require('mongoose')
-const dotenv = require("dotenv")
-
+const mongoose = require( "mongoose")
+const dotenv = require ( "dotenv")
 dotenv.config()
-  
-  mongoose.connect(process.env.DATABASE_URL)
-  .then(() => {
-      const userSchema = new mongoose.Schema({
-          _id : String,
-          src: String
-      });
-  
-      // User 모델 생성
-      const musicData = mongoose.model('staticLink', userSchema,'staticLink');
-  
-      // 사용자 데이터 불러오기
-      musicData.find({})
-          .then(users => {
-              console.log('모든 사용자 데이터:', users);
-              return users
-          })
-          .catch(error => {
-              console.error('데이터 조회 오류:', error);
-          });
-  })
-  .catch((error) => {
-      console.error('MongoDB 연결 오류:', error);
-  });
 
+const schema = new mongoose.Schema({
+  _id : String,
+  src : String,
+})
+
+const mongooseData = async ()=>{
+
+  const dataModel = mongoose.model("MusicData", schema, "MusicData")
+  try {
+    await mongoose.connect(process.env.DATABASE_URL)
+    const result = await dataModel.find({})
+    console.log(`result Data : ${result}`)
+    console.log(Array.isArray(result))
+    return result
+    
+  } catch (error) {
+
+    console.log(`error occur : ${error}`)
+  } 
+}
+
+mongooseData()
