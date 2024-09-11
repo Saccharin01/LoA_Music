e4f4124ac0bc6044be20c9c12013c6cc9f222ceb
조우식 <chousik01@gmail.com>
2024-09-11 11:36:55 +0900

    component DropBox

--- loa_music/src/app/components/DropBox.tsx
+++ loa_music/src/app/components/DropBox.tsx
@@ -1,8 +1,20 @@
-export default function DropBox(){
+"use client"
+
+import { useEffect } from "react"
+import { useDragDrop } from "./context/useDragDrop"
+
+export default function DropBox() {
+  const { droppedItem, setDroppedItem } = useDragDrop(); // 객체 구조 분해 할당으로 데이터 추출
+
+  useEffect(() => {
+    if (droppedItem) {
+      console.log('Dropped Item:', droppedItem); // 드래그된 아이템이 있을 때만 로그 출력
+    }
+  }, [droppedItem]); // droppedItem이 변경될 때마다 useEffect 실행
+
   return (
-    <div
-    className="flex justify-center items-center border-4 border-dashed w-2/3 h-2/3  rounded-2xl">
+    <div className="flex justify-center items-center border-4 border-dashed w-2/3 h-2/3 rounded-2xl">
       Drag Here!
     </div>
-  )
+  );
 }
