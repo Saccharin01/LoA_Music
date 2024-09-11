1828e097e63e9058db035da75e5849e68293fff2
조우식 <chousik01@gmail.com>
2024-09-11 11:35:39 +0900

    implant DragDrop hooks

--- loa_music/src/app/components/DragElement.tsx
+++ loa_music/src/app/components/DragElement.tsx
@@ -2,11 +2,15 @@
 
 import { useEffect, useRef } from "react";
 import { useData } from "./context/useData"; // 커스텀 훅 import
-import mongooseData from "../modules/MongooseData"
+import mongooseData from "../modules/MongooseData";
+import { useDragDrop } from "./context/useDragDrop";
+import DataFormat from "@/shared/IDataFromat";
+
 
 export default function DragElement() {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const data = useData(); // 커스텀 훅 사용
+  const data = useData();
+  const { setDroppedItem } = useDragDrop();
 
   useEffect(() => {
     const container = containerRef.current;
@@ -35,6 +39,12 @@ export default function DragElement() {
     fetchData();
   }, []);
 
+  // 드래그 시작 시 호출될 함수
+  const handleDragStart = (event: React.DragEvent<HTMLDivElement>, item: DataFormat) => {
+    event.dataTransfer.setData('application/json', JSON.stringify(item)); // 데이터를 데이터 전송 객체에 저장
+    setDroppedItem(item); // 컨텍스트 프로바이더에 데이터 저장
+  };
+
   return (
     <div
       ref={containerRef}
@@ -45,7 +55,7 @@ export default function DragElement() {
           key={index}
           className="min-w-60 m-3 h-40 flex justify-center items-center bg-indigo-400 cursor-pointer"
           draggable
-          onDragStart={()=>{console.log("draging!")}}
+          onDragStart={(event) => handleDragStart(event, element)} // 드래그 시작 핸들러 설정
         >
           {element._id}
         </div>
