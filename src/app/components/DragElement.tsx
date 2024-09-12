025292051b2dfcd6fca385f54ea39a297f49d237
조우식 <chousik01@gmail.com>
2024-09-12 12:31:42 +0900

    revert

--- src/app/components/DragElement.tsx
+++ src/app/components/DragElement.tsx
@@ -3,40 +3,21 @@
 import { useEffect, useRef } from "react";
 import { useData } from "./context/useData"; // 커스텀 훅 import
 import mongooseData from "../modules/MongooseData";
+import { useDragDrop } from "./context/useDragDrop";
 import DataFormat from "@/shared/IDataFromat";
 
 export default function DragElement() {
-  const data = useData();
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const data = useData();
+  // const { setDroppedItem } = useDragDrop();
 
   useEffect(() => {
     const container = containerRef.current;
-
     if (container) {
       const scrollEvent = (event: WheelEvent) => {
         event.preventDefault();
-
-        let scrollAmount = event.deltaY;
-
-        // deltaMode에 따라 스크롤 속도 조정
-        if (event.deltaMode === WheelEvent.DOM_DELTA_LINE) {
-          scrollAmount *= 16; // 라인당 16px로 조정
-        } else if (event.deltaMode === WheelEvent.DOM_DELTA_PAGE) {
-          scrollAmount *= container.clientWidth; // 페이지 단위 스크롤
-        }
-
-        // 트랙패드인지 확인하여 스크롤 속도 조정 (MacOS)
-        const isTrackpad = Math.abs(event.deltaY) < 100;
-
-        if (isTrackpad) {
-          scrollAmount *= 0.5; // 트랙패드 스크롤을 덜 민감하게
-        } else {
-          scrollAmount *= 1; // 마우스 휠의 경우 기본 속도 유지
-        }
-
-        container.scrollLeft += scrollAmount; // 가로 스크롤 적용
+        container.scrollLeft += event.deltaY;
       };
-
       container.addEventListener("wheel", scrollEvent);
 
       return () => {
@@ -62,15 +43,28 @@ export default function DragElement() {
     event.dataTransfer.setData('application/json', JSON.stringify(item)); // 데이터를 데이터 전송 객체에 저장
   };
 
+  // // 드래그 오버 시 호출될 함수
+  // const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+  //   event.preventDefault(); // 드롭을 허용하기 위해 기본 동작을 방지
+  // };
+
+  // // 드롭 시 호출될 함수
+  // const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+  //   event.preventDefault();
+  //   const data = event.dataTransfer.getData('application/json');
+  //   const item = JSON.parse(data);
+  //   setDroppedItem(item); // 컨텍스트 프로바이더에 데이터 저장
+  // };
+
   return (
     <div
       ref={containerRef}
-      className="w-full h-50 overflow-x-auto overflow-y-hidden whitespace-nowrap"
+      className="w-full h-50 overflow-hidden flex justify-start items-cen ter transition-all duration-300 ease-in-out"
     >
       {data.map((element, index) => (
         <div
           key={index}
-          className="inline-block min-w-60 m-3 h-40 justify-center items-center bg-indigo-400 cursor-pointer"
+          className="min-w-60 m-3 h-40 flex justify-center items-center bg-indigo-400 cursor-pointer"
           draggable
           onDragStart={(event) => handleDragStart(event, element)} // 드래그 시작 핸들러 설정
         >
