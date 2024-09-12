4c4a17a7f6dde8dda7a92e534a85befe9e3771fa
조우식 <chousik01@gmail.com>
2024-09-12 11:50:56 +0900

    feat | Data.dto.ts
    추가될 데이터들을 명시하고 분리된 부분의 병합 준비

--- src/shared/Data.dto.ts
+++ src/shared/Data.dto.ts
@@ -1,5 +1,14 @@
 import { Document } from "mongoose"
 
+export default interface DataFormat {
+  _id : string,
+  src : string,
+  kr_name : string,
+  thumbnail : string,
+  description : string
+}
+
+
 export default interface MusicData extends Document {
   _id : string,
   src : string
