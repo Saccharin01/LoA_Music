8cb6997d08669ce3f9b858178e8295f42e8593c5
Saccharin01 <chousik01@gmail.com>
2024-09-30 06:49:57 +0900

    feat | next.config.mjs
    이미지 호스팅 주소 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['lomusic2.s3.amazonaws.com', 'lomusic2.s3.ap-northeast-2.amazonaws.com'], // 허용할 도메인 추가
+  }
+}
 
 export default nextConfig;
