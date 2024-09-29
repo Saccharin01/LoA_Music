2a3c6cd40fa2d5d42bb46e4377d94ae53bc7e053
Saccharin01 <chousik01@gmail.com>
2024-09-30 06:51:43 +0900

    new | page.tsx (About)
    상세 페이지 스켈레톤 추가.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+import AboutPage from "../components/AboutPage";
+import { useState } from "react";
+import updateLink from "@/shared/link";
+
+export default function About() {
+  const [linkData, setLinkData] = useState(updateLink);
+
+  return (
+    <div className="bg-slate-300 mt-20 h-[96vh] w-[96vw]">
+      {linkData.map((link) => (
+        <AboutPage key={link.label} updateLink={[link]} />
+      ))}
+    </div>
+  );
+}
