eacf5f32fb0b16dc1f54f27911f271ae632148b9
Saccharin01 <chousik01@gmail.com>
2024-09-30 06:50:46 +0900

    feat | layout.tsx
    layout 에서 분리된 컴포넌트로 미사용 모듈 트리셰이킹

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import DragElement from "./components/DragElement";
 import { DataProvider } from "./components/context/useData";
 import { DragDropProvider } from "./components/context/useDragDrop";
-import MusicPlayer from "./components/MusicPlayer";
-import DropBox from "./components/DropBox";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
@@ -19,13 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="w-full h-full">
+      <body className="bg-[#282c2c]">
         <DataProvider>
           <DragDropProvider>
-            <header className="absolute w-full top-0 left-0 z-[1]">
+            <header className="absolute w-full top-0 left-0 z-[1] h-16">
               <Navbar />
             </header>
-            {children}
+            <div className="mt-16">
+              {children}
+            </div>
           </DragDropProvider>
         </DataProvider>
       </body>
