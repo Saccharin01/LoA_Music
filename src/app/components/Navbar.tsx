486cc13cd37aa7b9a77cabd43cd023e98c0cc6ae
Saccharin01 <chousik01@gmail.com>
2024-09-29 20:31:38 +0900

    style | NavBar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -18,27 +18,31 @@ export default function Navbar() {
         <div className="w-screen fixed z-50 bg-[#282c2c]">
           <div className="flex justify-between h-16 px-10 shadow items-cente">
             <div className="flex items-center space-x-1">
-              <h1 className="text-xl lg:text-2xl font-bold cursor-pointer mr-3">
-                LoA Music
-              </h1>
+              <div>
+                <Link href="/" className="">
+                  <h1 className="text-xl lg:text-2xl font-bold cursor-pointer mr-3 border-cyan-300 text-slate-200 hover:text-yellow-500">
+                    LoA Music
+                  </h1>
+                </Link>
+              </div>
               <div className="hidden lg:flex justify-around space-x-4 gap-10">
                 <Link
-                  href="test"
-                  className="hover:text-yellow-500 text-slate-200 pl-10"
+                  href="/"
+                  className="hover:text-yellow-500 text-slate-200 pl-10 font-bold"
                 >
                   Home
                 </Link>
                 <Link
-                  href="test"
-                  className="hover:text-yellow-500 text-slate-200"
+                  href="music"
+                  className="hover:text-yellow-500 text-slate-200 font-bold"
                 >
-                  About
+                  Music
                 </Link>
                 <Link
-                  href="test"
-                  className="hover:text-yellow-500 text-slate-200"
+                  href="about"
+                  className="hover:text-yellow-500 text-slate-200 font-bold"
                 >
-                  ADMIN
+                  About
                 </Link>
               </div>
             </div>
