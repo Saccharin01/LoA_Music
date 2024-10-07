1a3ccae62fb95af206c592ea7357740cf3e28c41
Saccharin01 <chousik01@gmail.com>
2024-10-08 05:48:13 +0900

    feat | Description.tsx
    타입 가드를 통해 예외처리 및 데이터를 불러올 수 있는 GenreNav 추가

--- src/app/components/Description.tsx
+++ src/app/components/Description.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import { useDragDrop } from "./context/useDragDrop";
+import GenreNav from "./GenreNav";
 
 export default function MusicDescription() {
   const { droppedItem } = useDragDrop();
 
   if (!droppedItem || !droppedItem.description) {
-    return <div className="p-4">No dropped item available.</div>;
+    return (
+      <div className="grid grid-cols-1 gap-4 p-4 rounded-lg shadow-md h-[90%] mt-[px]">
+        <div className="bg-orange-100 p-4 rounded-lg shadow-md">
+          <h2 className="text-xl font-bold mb-2">불러온 내용이 없어요</h2>
+          <p className="text-sm text-gray-700">아래의 버튼을 이용해서 장르를 선택 해주세요</p>
+        </div>
+        <div className="bg-gray-100 p-4 rounded-lg shadow-md flex justify-center items-center">
+          <GenreNav />
+        </div>
+      </div>
+    );
   }
 
   const { headLine, mainScript } = droppedItem.description;
@@ -17,8 +28,8 @@ export default function MusicDescription() {
         <h2 className="text-xl font-bold mb-2">{headLine}</h2>
         <p className="text-sm text-gray-700">{mainScript}</p>
       </div>
-      <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-        <p>some Tag will here</p>
+      <div className="bg-gray-100 p-4 rounded-lg shadow-md flex justify-center items-center">
+        <GenreNav />
       </div>
     </div>
   );
