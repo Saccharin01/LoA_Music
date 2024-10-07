7ee1a8f0c3ffabc8fd650f0e7cdcc17d9810e0fa
조우식 <chousik01@gmail.com>
2024-10-07 17:59:25 +0900

    feat | Navbar.tsx
    네비게이션 바 수정. 메인 페이지에서 사용하고자 한다면 다른 식으로 라우팅 테이블을 생성해야 하나, 현 상황에서는 Loa_Music 에서만 사용할 의항이기 때문에 강행

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,45 +1,40 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const path = usePathname();
 
-  // useEffect(() => {
-  //   if (path === "/") setIsView(false);
-  //   else setIsView(true);
-  // }, [path]);
 
   return (
     <>
       <nav>
+        
         <div className="w-screen fixed z-50 bg-[#3e3e3f]">
           <div className="flex justify-between h-16 px-10 shadow items-cente">
             <div className="flex items-center space-x-1">
               <div>
-                <Link href="/" className="">
-                  <h1 className="text-xl lg:text-2xl font-bold cursor-pointer mr-3 border-cyan-300 text-slate-200 ">
+                <Link href="/music" className="">
+                  <h1 className="text-xl lg:text-2xl font-bold cursor-pointer mr-10 border-cyan-300 text-slate-200 ">
                     LoA Music
                   </h1>
                 </Link>
               </div>
               <div className="hidden lg:flex justify-around space-x-4 gap-10">
-                <Link
+                {/* <Link
                   href="/"
                   className="hover:text-yellow-500 text-slate-200 pl-10 font-bold"
                 >
                   Home
-                </Link>
+                </Link> */}
                 <Link
-                  href="music"
+                  href="/music/player"
                   className="hover:text-yellow-500 text-slate-200 font-bold"
                 >
-                  Music
-                </Link>
+                  player
+                </Link> 
                 <Link
-                  href="about"
+                  href="/music/about"
                   className="hover:text-yellow-500 text-slate-200 font-bold"
                 >
                   About
