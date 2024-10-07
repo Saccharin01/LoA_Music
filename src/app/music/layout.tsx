9a83c6b3f6c2fc2d7fd6c42f68733c83d1d8dde4
조우식 <chousik01@gmail.com>
2024-10-07 18:01:46 +0900

    new | layout.tsx / music
    music 페이지에서 사용하는 layout 정의

---
+++ src/app/music/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { DataProvider } from "../components/context/useData";
+import { DragDropProvider } from "../components/context/useDragDrop";
+import "../globals.css"
+import Navbar from "../components/Navbar";
+
+export const metadata: Metadata = {
+  title: "LoA Music",
+  description: "Losk Ark Theme music",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="bg-[#282c2c]">
+      <DataProvider>
+        <DragDropProvider>
+          <header className="absolute w-full top-0 left-0 z-[1] h-16">
+            <Navbar />
+          </header>
+          <div className="mt-16">{children}</div>
+        </DragDropProvider>
+      </DataProvider>
+    </div>
+  );
+}
