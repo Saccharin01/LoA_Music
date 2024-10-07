c274dcd8cc864163593d3a31ae4dee58649bee16
Saccharin01 <chousik01@gmail.com>
2024-10-08 04:01:10 +0900

    feat | list.ts
    S3 버킷의 내용을 읽어오고  json으로 만드는 코드 수정

--- AWS_files/src/list.ts
+++ AWS_files/src/list.ts
@@ -1,12 +1,18 @@
 import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 import configClass from "./modules/configClass";
-import fs from "fs"
+import fs from "fs";
 
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
+interface IContainer {
+  _id: string;
+  img: string;
+  src: string;
+  description: string;
+  genre: string;
+}
 
 const listFilesInBucket = async (bucketName: string) => {
   try {
@@ -17,19 +23,32 @@ const listFilesInBucket = async (bucketName: string) => {
     const command = new ListObjectsV2Command(params);
     const data = await configClass.send(command);
 
-    const files: { [key: string]: string } = {}; // 파일 이름을 key로, URL을 value로 가지는 객체
+    const result: IContainer[] = [];
 
     data.Contents?.forEach((item) => {
+      const fileContainer: IContainer = {
+        _id: "",
+        img: "",
+        src: "",
+        description: "",
+        genre: "",
+      };
+
       if (item.Key) {
-        const fileName = item.Key.split(".")[0]// 파일 이름만 추출
+        const fileName = item.Key.split(".")[0]; // 파일 이름만 추출
+        const extension = item.Key.split(".")[1];
         const url = `https://${bucketName}.s3.amazonaws.com/${encodeURIComponent(item.Key)}`;
-        if (fileName) {
-          files[fileName] = url;
+        fileContainer._id = fileName
+        if (extension === "mp3") {
+          fileContainer.src = url;
+        } else {
+          fileContainer.img = url;
         }
+        result.push(fileContainer)
       }
     });
 
-    const jsonOutput = JSON.stringify(files, null, 2);
+    const jsonOutput = JSON.stringify(result, null, 2);
     console.log(jsonOutput);
 
     // JSON 데이터를 파일로 기록하기 (비동기 방식)
@@ -43,7 +62,6 @@ const listFilesInBucket = async (bucketName: string) => {
     });
 
     return jsonOutput;
-
   } catch (err) {
     console.error("Error listing objects in bucket:", err);
   }
