67a586e0c8b7b8dfd7c1a570839a885d20ff07f8
조우식 <chousik01@gmail.com>
2024-10-07 17:58:09 +0900

    new | layout.tsx
    페이지 네스팅. layout을 분리하기 위해 메인 페이지에서 사용하는 레이아웃 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-import { DataProvider } from "./components/context/useData";
-import { DragDropProvider } from "./components/context/useDragDrop";
 import "./globals.css";
-import Navbar from "./components/Navbar";
 
 export const metadata: Metadata = {
-  title: "LoA Music",
-  description: "Losk Ark Theme music",
+  title: "조우식",
+  description: "조우식의 페이지 입니다.",
 };
 
 export default function RootLayout({
@@ -16,17 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="bg-[#282c2c]">
-        <DataProvider>
-          <DragDropProvider>
-            <header className="absolute w-full top-0 left-0 z-[1] h-16">
-              <Navbar />
-            </header>
-            <div className="mt-16">
+      <body>
+            <div>
               {children}
             </div>
-          </DragDropProvider>
-        </DataProvider>
       </body>
     </html>
   );
