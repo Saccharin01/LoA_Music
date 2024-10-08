368e31504e4ccb3a46f46a59f268bc680958189d
조우식 <chousik01@gmail.com>
2024-10-08 17:27:28 +0900

    test deploy

--- src/app/api/musicData/route.ts
+++ src/app/api/musicData/route.ts
@@ -2,36 +2,28 @@ import { NextResponse } from 'next/server';
 import mongoose from 'mongoose';
 import MusicData from './model/mongooseModel';
 
-// Next.js의 환경 변수 접근 방식 사용
-const DATABASE_URL = process.env.DATABASE_URL;
+// 이 라우트는 동적으로 처리되어야 함을 명시
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
   try {
     const url = new URL(request.url);
     const genre = url.searchParams.get('genre');
 
-    // 환경 변수 확인을 위한 로깅
-    console.log('Database URL:', DATABASE_URL);
-    
-    if (!DATABASE_URL) {
-      throw new Error('Database URL is not defined');
-    }
-
     // 데이터베이스 연결
     if (mongoose.connection.readyState !== 1) {
-      await mongoose.connect(DATABASE_URL);
+      await mongoose.connect(process.env.DATABASE_URL as string);
     }
 
-    const query = {genre: genre}
+    const query = {genre : genre}
     const data = await MusicData.find(query);
 
     return NextResponse.json(data);
   } catch (error) {
-    console.error('Error details:', error);
-    
+    console.error(error)
     return NextResponse.json({
-      error: 'Error fetching data',
-      details: error instanceof Error ? error.message : 'Unknown error'
+       error: 'Error fetching data',
+      errorObj : error
     }, { status: 500 });
   }
 }
