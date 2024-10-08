deca0f9b013a779447bbb6096ea2dc0045c6f457
조우식 <chousik01@gmail.com>
2024-10-08 14:46:34 +0900

    환경변수로 경로 처리 명시화

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   images: {
     domains: ['lomusic2.s3.amazonaws.com', 'lomusic2.s3.ap-northeast-2.amazonaws.com'], // 허용할 도메인 추가
-  }
+  },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://saccharin01.com/',
+  },
 }
 
 export default nextConfig;
