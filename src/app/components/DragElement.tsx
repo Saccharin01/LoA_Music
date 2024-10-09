5cd7c2cf2c037fc5d6d45001fde16ac300588529
Saccharin01 <chousik01@gmail.com>
2024-10-10 08:45:01 +0900

    최적화 진행 중

--- src/app/components/DragElement.tsx
+++ src/app/components/DragElement.tsx
@@ -1,40 +1,40 @@
 "use client";
-import Image from "next/image";
+import React, { useMemo, useCallback } from "react";
 import { useData } from "./context/useData";
+import ImageItem from "./memo/ImageItem";
 import { IMusicDataFormat } from "@/shared/IDataFromat";
 
 export default function DragElement() {
   const { data, loading } = useData();
 
-  const handleDragStart = (
-    event: React.DragEvent<HTMLDivElement>,
-    item: IMusicDataFormat
-  ) => {
-    if (event.target instanceof HTMLImageElement)
-      event.dataTransfer.setData("application/json", JSON.stringify(item));
-  };
+  const handleDragStart = useCallback(
+    (event: React.DragEvent<HTMLDivElement>, item: IMusicDataFormat) => {
+      if (event.target instanceof HTMLImageElement)
+        event.dataTransfer.setData("application/json", JSON.stringify(item));
+    },
+    []
+  );
 
-  return (
+  const imageList = useMemo(
+    () =>
+      data.map((element, index) => (
+        <ImageItem
+          key={index}
+          element={element}
+          handleDragStart={handleDragStart}
+        />
+      )),
+    [data, handleDragStart]
+  );
+
+  if (!loading) {
+    return (
     <div className="w-full h-full overflow-x-hidden hide-scrollbar">
       <div className="grid grid-cols-5 gap-2 place-items-center overflow-auto">
-        {data.map((element, index) => (
-          <div
-            key={index}
-            className="flex justify-center items-center m-2 h-40 cursor-pointer w-80"
-            draggable
-            onDragStart={(event) => handleDragStart(event, element)}
-          >
-            <Image
-              src={element.img}
-              alt={element._id}
-              className="h-full"
-              width={300}
-              height={160}
-              loading="lazy"
-            />
-          </div>
-        ))}
+        {imageList}
       </div>
     </div>
-  );
+    )
+  }
+
 }
---
+++ src/app/components/memo/ImageItem.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import { IMusicDataFormat } from "@/shared/IDataFromat";
+
+interface ImageItemProps {
+  element: IMusicDataFormat;
+  handleDragStart: (event: React.DragEvent<HTMLDivElement>, item: IMusicDataFormat) => void;
+}
+
+const ImageItem: React.FC<ImageItemProps> = ({ element, handleDragStart }) => {
+  return (
+    <div
+      className="flex justify-center items-center m-2 h-40 cursor-pointer w-80"
+      draggable
+      onDragStart={(event) => handleDragStart(event, element)}
+    >
+      <Image
+        src={element.img}
+        alt={element._id}
+        className="h-full"
+        width={300}
+        height={160}
+        loading="lazy"
+      />
+    </div>
+  );
+};
+
+export default ImageItem;
