58389ef44086303a5cb1c8985105e34c0aecf417
Saccharin01 <chousik01@gmail.com>
2024-10-10 05:56:22 +0900

    feat | next.config.mjs
    이미지 컴포넌트 처리 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lomusic2.s3.amazonaws.com', 'lomusic2.s3.ap-northeast-2.amazonaws.com'],
+    // remotePatterns: [
+    //   "lomusic2.s3.amazonaws.com",
+    //   "lomusic2.s3.ap-northeast-2.amazonaws.com",
+    // ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lomusic2.s3.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lomusic2.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
   },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
-  }
-}
+  },
+};
 
 export default nextConfig;
