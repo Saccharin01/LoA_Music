1a7bd8fbea5bbeb37eab2e663db7de03c7752526
Saccharin01 <chousik01@gmail.com>
2024-10-11 22:14:52 +0900

    testing

--- src/app/api/modules/InitalizeServerENV.ts
+++ src/app/api/modules/InitalizeServerENV.ts
@@ -11,20 +11,19 @@ export async function InitalizeServerENV(
   try {
     await connectDB();
     const newestID = await LatestID(requestModel);
-    globalThis.Memo = newestID;
-
+    globalThis.Memo = newestID;  // 서버 기동 시 최신 요청 로그의 _id 값을 초기화
     console.log(`Newest Doc's _id value : ${newestID}`);
 
     if (!globalThis.cache) {
       globalThis.cache = new LRUCache<string, IMailTimestemp>(10); 
-      console.log('LRUCache가 성공적으로 초기화되었습니다.');
+      console.log('LRUCache가 성공적으로 초기화되었습니다.', globalThis.cache );
     }
 
-    const lastEmailEvent = await fetchLastEmailEvent(mailModel);
+    const lastEmailEvent = await fetchLastEmailEvent(mailModel); // 메일 전송 이벤트 조회
 
     if (lastEmailEvent) {
-      globalThis.cache.set("lastEmailEvent", lastEmailEvent);
-      console.log(`캐시 초기화 완료: 메일 이벤트가 로드되었습니다.`);
+      globalThis.cache.set("lastEmailEvent", lastEmailEvent); // LRU 캐시에 마지막 메일 전송 이벤트 저장
+      console.log(`캐시 초기화 완료: 메일 이벤트가 로드되었습니다.`, lastEmailEvent);
     }
   } catch (error) {
     console.error("서버 시작 시 초기화 오류:", error);
@@ -35,7 +34,7 @@ export async function fetchLastEmailEvent(
   model: Model<IMailTimestempServer>
 ): Promise<IMailTimestempServer | null> {
   try {
-    const lastEvent = await model.findOne().sort({ sentTime: -1 }).exec();
+    const lastEvent = await model.findOne().sort({ sentTime: -1 }).exec(); // 가장 최근 메일 전송 이벤트 조회
     return lastEvent;
   } catch (error) {
     console.error("Error fetching last email event:", error);
@@ -54,7 +53,7 @@ export async function LogMailEvent(
       status: object.status,
     });
 
-    await newEvent.save();
+    await newEvent.save(); // 메일 전송 이벤트를 데이터베이스에 저장
     console.log(`메일 전송 이벤트가 데이터베이스에 저장되었습니다.`);
   } catch (error) {
     console.error("Error saving mail event to database:", error);
