a93328a01cbcfae08456b5f4ba03c74cccb14bf7
조우식 <chousik01@gmail.com>
2024-10-11 18:26:26 +0900

    feat | route.ts / request
    POST 요청에 대한 서버 코드 업데이트 중. 캐시 생성 및 모듈화 작업 점검 중

--- src/app/api/request/route.ts
+++ src/app/api/request/route.ts
@@ -1,35 +1,26 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
-import { initializeLatestId } from "../modules/initializeLatestId";
+import { InitalizeServerENV, LogMailEvent } from "../modules/InitalizeServerENV";
 import connectDB from "../modules/ConnectDB";
-import { RequestLog } from "../model/mongooseModel";
+import { RequestLog, MailTimestemp } from "../model/mongooseModel";
 import { IRequestDataFormat } from "@/shared/IDataFromat";
-import { getDate, numberdId, validateRequestData } from "../modules/RequestFormat";
+import { getDate, numberdId, validateRequestData} from "../modules/RequestFormat";
 import MailNotification from "../modules/MailNotification";
 
-let Memo = 0; 
-const REQUEST_THRESHOLD = 30;
-console.log(process.env.EMAIL_USER)
-console.log(process.env.EMAIL_PASS)
+const REQUEST_THRESHOLD = 10;
+
+InitalizeServerENV(RequestLog, MailTimestemp);
+
 
 export async function POST(request: Request) {
+  console.log(globalThis.cache);
+  console.log(globalThis.Memo);
   try {
-    await connectDB(); 
-    if (Memo === 0) {
-      initializeLatestId(RequestLog)
-        .then((latestId : number) => {
-          Memo = latestId;
-        })
-        .catch((error : Error) => {
-          console.error("최신 _id 초기화 오류:", error);
-        });
-    }
-    
+    await connectDB(); // 각 요청마다 데이터베이스 연결 상태 확인
+
     const body: IRequestDataFormat = await request.json();
-
-    validateRequestData(body.requestLog);
-
+    validateRequestData(body.requestLog); // 요청 데이터 검증
 
     const lastRequest = await RequestLog.findOne().sort({ _id: -1 }).exec();
     const newId = numberdId(lastRequest);
@@ -43,14 +34,30 @@ export async function POST(request: Request) {
     await newRequestLog.save();
 
     const numericCurrentId = parseInt(newId.split(".")[1], 10);
-    const idDifference = numericCurrentId - Memo;
+    const idDifference = numericCurrentId - globalThis.Memo;
 
     if (idDifference >= REQUEST_THRESHOLD) {
-      MailNotification(idDifference).catch((error : Error) => {
+      try {
+        // 메일 전송 시도
+        MailNotification(idDifference);
+        const MailLogID = await MailTimestemp.findOne().sort({ _id: -1 }).exec();
+        // 메일이 성공적으로 전송된 경우에만 메일 이벤트를 로그로 기록
+        await LogMailEvent(
+          {
+            _id: numberdId(MailLogID),
+            sentTime: new Date(),
+            status: "sent",
+          },
+
+          MailTimestemp
+        );
+
+        console.log("메일 전송 및 로그 기록 완료");
+      } catch (error) {
         console.error("Error sending email:", error);
-      });
-      // todo 배포 전에 실행 시킬 것 !!!! 
-      Memo = numericCurrentId;
+      }
+
+      globalThis.Memo = numericCurrentId; // 최신 ID 업데이트
     }
 
     return NextResponse.json(
@@ -58,8 +65,8 @@ export async function POST(request: Request) {
       { status: 201 }
     );
   } catch (error) {
-    // 외부 try-catch 블록에서 모든 예외 처리
-    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
     console.error("Request error:", error);
     return NextResponse.json(
       {
