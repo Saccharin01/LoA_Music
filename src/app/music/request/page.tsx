e579f5ee8174cca0c75e93969b4ef7903a982c3d
조우식 <chousik01@gmail.com>
2024-10-14 15:17:56 +0900

    feat | page.tsx / request
    입력 값 검증하는 함수 모듈로 분리

--- src/app/music/request/page.tsx
+++ src/app/music/request/page.tsx
@@ -1,61 +1,20 @@
 "use client";
-import { useRef, useEffect } from "react";
-import validator from "validator";
+import { useRef } from "react";
+import Validate from "./modules/Validate";
 
 export default function RequestPage() {
   const requestLogRef = useRef<HTMLInputElement>(null);
 
-  const handleSubmit = async () => {
-    const requestLog = requestLogRef.current?.value.trim() || "";
-
-    if (!validator.isLength(requestLog, { min: 1, max: 100 })) {
-      alert("입력값은 1자 이상 100자 이하여야 합니다.");
-      return;
-    }
-
-    if (
-      validator.contains(requestLog, "<") ||
-      validator.contains(requestLog, ">")
-    ) {
-      alert("입력 값에 허용되지 않는 문자열이 존재합니다.");
-      return;
-    }
-
-    try {
-      const response = await fetch("/api/request", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ requestLog, date: new Date() }),
-      });
-
-      if (response.ok) {
-        alert("요청이 성공적으로 전송되었습니다.");
-        if (requestLogRef.current) {
-          requestLogRef.current.value = "";
-        }
-      } else {
-        alert("요청 전송에 실패했습니다.");
-      }
-    } catch (error) {
-      console.error("Error sending request:", error);
-      alert("요청 전송 중 오류가 발생했습니다.");
-    }
+  
+  const handleSubmit = () => {
+    Validate(requestLogRef, "/api/request");
   };
 
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Enter") {
-        handleSubmit();
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSubmit(); 
+    }
+  };
 
   return (
     <div className="w-full h-full">
@@ -65,6 +24,7 @@ export default function RequestPage() {
           name="requestLog"
           placeholder="요청사항입니다"
           type="text"
+          onKeyDown={handleKeyDown} 
         />
       </div>
       <div>
