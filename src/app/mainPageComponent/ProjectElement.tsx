1f41f96f5e37fe29d2c5fc0eb69046b9f78942b2
조우식 <chousik01@gmail.com>
2024-10-23 19:42:34 +0900

    new | ProjectElement.tsx
    프로젝트 요소를 그리는 DOM 요소 컴포넌트입니다.
    이 컴포넌트에서는 static 데이터를 context 로 끌어올려 다른 컴포넌트에서 해당 데이터를 구독 할 수 있도록 조정하고 있습니다.

---
+++ src/app/mainPageComponent/ProjectElement.tsx
@@ -0,0 +1,49 @@
+import { useLiteral } from "./context/useLiteral";
+import { IstaticData } from "@/shared/static.interface";
+import { staticData } from "@/shared/static.data";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ProjectElement() {
+  const { setLiteral } = useLiteral(); 
+
+  const handleSelectData = (item: IstaticData) => {
+    setLiteral([item]); 
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <h1>Projects List</h1>
+      {staticData.map((element, index) => (
+        <div key={index} onClick={() => handleSelectData(element)}>
+          <Link href="/details">
+              <Image
+                src={element.img}
+                alt="이미지입니닥!"
+                width={300}
+                height={400}
+                style={{ width: "auto", height: "auto" }}
+                priority
+              />
+              <div>
+                <h2>{element.title}</h2>
+                <p>{element.description}</p>
+                <p>{element.details.introduce}</p>
+              </div>
+          </Link>
+          <div>
+            {element.stack.map((stack, idx) => (
+              <span
+                key={idx}
+                className="rounded-full bg-purple-900/50 px-2 py-0.5 text-xs text-purple-300 md:px-3 md:py-1 md:text-sm"
+              >
+                {stack}
+              </span>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
