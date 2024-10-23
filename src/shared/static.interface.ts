850db3a010ea469cb44abf49d91ea599fdcec315
조우식 <chousik01@gmail.com>
2024-10-23 19:43:52 +0900

    new | static.interface.ts
    정적 데이터의 형식을 지정한 인터페이스입니다

---
+++ src/shared/static.interface.ts
@@ -0,0 +1,13 @@
+export interface IstaticData {
+  title: string;
+  description: string;
+  stack: string[];
+  img : string
+  details: {
+    introduce: string;
+    about: {
+      placeHolder: string;
+      link: string;
+    };
+  };
+}
