5b9528b4d3322ebaeb64e0baff0068f338fddc48
조우식 <chousik01@gmail.com>
2024-10-23 19:44:49 +0900

    new | static.data.ts
    정적 데이터를 정의해놓은 파일입니다. 결국 이 값은 json이 될 것이고 데이터베이스에서 페칭으로 받아오는 데이터가 되겠지만, 신속하게 그려내기 위해 우선적으로 데이터를 정적으로 선언합니다.

---
+++ src/shared/static.data.ts
@@ -0,0 +1,46 @@
+import { IstaticData } from "./static.interface";
+
+export const staticData: IstaticData[] = [
+  {
+    title: "Good Start",
+    description:
+      "HTML, CSS, JS를 핸들링 해보며 이벤트를 익히기 위해 작성한 html, js",
+    stack: ["HTML", "CSS", "JavaScript"],
+        img : "https://lomusic2.s3.amazonaws.com/An_Orgel_of_Greatest_Memory.webp",
+    details: {
+      introduce: "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
+      about: {
+        placeHolder: "notion",
+        link: "",
+      },
+    },
+  },
+  {
+    title: "CyberFunc ERP",
+    description:
+      "HTML, CSS, JS를 핸들링 해보며 이벤트를 익히기 위해 작성한 html, js",
+    stack: ["Next", "Nest", "MonogoDB Atlas", "TanStack"],
+    img : "https://lomusic2.s3.amazonaws.com/An_Orgel_of_Greatest_Memory.webp",
+    details: {
+      introduce: "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
+      about: {
+        placeHolder: "notion",
+        link: "",
+      },
+    },
+  },
+  {
+    title: "Taison Tiger",
+    description:
+      "HTML, CSS, JS를 핸들링 해보며 이벤트를 익히기 위해 작성한 html, js",
+    stack: ["HTML", "CSS", "JavaScript"],
+    img : "https://lomusic2.s3.amazonaws.com/An_Orgel_of_Greatest_Memory.webp",
+    details: {
+      introduce: "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
+      about: {
+        placeHolder: "notion",
+        link: "",
+      },
+    },
+  },
+];
