122a9e85b7b488938c2399c0f4bd120f7d13d052
Saccharin01 <chousik01@gmail.com>
2024-10-24 14:08:27 +0900

    feat | static.data.ts
    데이터 추가 중

--- src/shared/static.data.ts
+++ src/shared/static.data.ts
@@ -1,4 +1,4 @@
-import { IstaticData } from "../app/components/interface/static.interface";
+import { IstaticData } from "./interface/static.interface";
 
 export const staticData: IstaticData[] = [
   {
@@ -6,10 +6,11 @@ export const staticData: IstaticData[] = [
     description:
       "HTML, CSS, JS를 핸들링 해보며 이벤트를 익히기 위해 작성한 html, js",
     stack: ["HTML", "CSS", "JavaScript"],
-    img: "https://lomusic2.s3.amazonaws.com/An_Orgel_of_Greatest_Memory.webp",
+    img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/GoodStart/goodStart_SampleIMG.webp",
     details: {
       introduce:
         "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
+      params : "",
       about: [
         {
           placeholder: "notion",
@@ -23,11 +24,12 @@ export const staticData: IstaticData[] = [
     description:
       "HTML, CSS, JS를 핸들링 해보며 이벤트를 익히기 위해 작성한 html, js",
     stack: ["Next", "Nest", "MonogoDB Atlas", "TanStack"],
-    img: "https://lomusic2.s3.amazonaws.com/An_Orgel_of_Greatest_Memory.webp",
+    img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/CyberFunc/cyberfunc_index.webp",
     details: {
       introduce:
         "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
-      about: [
+        params : "",
+        about: [
         {
           placeholder: "notion",
           link: "",
@@ -40,11 +42,12 @@ export const staticData: IstaticData[] = [
     description:
       "HTML, CSS, JS를 핸들링 해보며 이벤트를 익히기 위해 작성한 html, js",
     stack: ["HTML", "CSS", "JavaScript"],
-    img: "https://lomusic2.s3.amazonaws.com/An_Orgel_of_Greatest_Memory.webp",
+    img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/taison/taison_index.webp",
     details: {
       introduce:
         "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
-      about: [
+        params : "",
+        about: [
         {
           placeholder: "notion",
           link: "",
@@ -57,18 +60,19 @@ export const staticData: IstaticData[] = [
     description:
       "팀 프로젝트를 진행하며 학습한 내용을 이용해서 스스로의 도메인으로 만드는 개인 프로젝트",
     stack: ['next','mongoDB Atlas',"AWS S3", "AWS Amplify"],
-    img: "https://lomusic2.s3.amazonaws.com/An_Orgel_of_Greatest_Memory.webp",
+    img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/LoA_Music/loaMusic_index.webp",
     details: {
       introduce:
         "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
-      about: [
+        params : "",
+        about: [
         {
           placeholder: "notion",
-          link: "",
+          link: "https://delicious-corn-899.notion.site/LoA-Music-123b1f89bb83810f9d9fd6506551536a",
         },
         {
           placeholder: "GitHub",
-          link: "",
+          link: "https://github.com/Saccharin01/LoA_Music",
         },
         {
           placeholder: "Deploy",
