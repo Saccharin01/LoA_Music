b463bc95b54a8c2f810758cd2955009c128ab5a9
조우식 <chousik01@gmail.com>
2024-10-24 19:54:54 +0900

    feat | page.tsx / details
    상세 페이지에서 사용하는 버튼, 스택 DOM 요소를 컴포넌트 분리.
    분리에 따른 모듈 임포트 및 적용

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -3,17 +3,19 @@
 import { useLiteral } from "../components/context/useLiteral";
 import Image from "next/image";
 import Link from "next/link";
-import ErrorPage from "../components/ErrorPage";
+import ErrorPage from "../components/ErrorPage"; 
+import ButtonGroup from "../components/ButtonGroup"; 
+import StackTags from "../components/StackTags";
 
 export default function DetailsPage() {
   const { literal } = useLiteral();
-  
+
   if (!literal) {
-    return <ErrorPage/>
+    return <ErrorPage />;
   }
-  
+
   const selectedProject = literal;
-  const innerObj = literal.details;
+  const innerObj = selectedProject.details;
 
   return (
     <div className="h-[100vh]">
@@ -24,28 +26,16 @@ export default function DetailsPage() {
           <p>{selectedProject.details.params}</p>
         </div>
 
-        <div>
-          <div className="flex flex-wrap gap-4 my-6">
-            {innerObj.about.map((data, index) => (
-              <Link
-                key={index}
-                href={data.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition"
-              >
-                {data.placeholder}
-              </Link>
-            ))}
+        <StackTags stackList={selectedProject.stack} />
 
-            <Link
-              href="/"
-              className="ml-auto bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300 transition"
-            >
-              뒤로가기
-            </Link>
-          </div>
-        </div>
+        <ButtonGroup aboutProps={innerObj.about} />
+
+        <Link
+          href="/"
+          className="ml-auto bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300 transition"
+        >
+          뒤로가기
+        </Link>
 
         <div className="flex justify-center my-4">
           <Image
