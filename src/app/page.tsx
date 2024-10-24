633bd5bc173acece505547f55490a48a1e6d0a10
조우식 <chousik01@gmail.com>
2024-10-24 18:50:17 +0900

    feat | page.tsx / main
    메인페이지에 스택 리스트 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
 "use client";
 import ProjectElement from "./components/ProjectElement";
+import StackElement from "./components/StackElement";
 
 export default function MainPage() {
   return (
     <>
       <header>
-        <div className="text-center max-w-4xl">
-          <h1 className="text-4xl font-bold mb-6">
-            반갑습니다. 이 곳은 조우식의 개인 페이지입니다.
+        <div className="max-w-4xl">
+          <h1 className="text-4xl font-bold mb-6 pt-6">
+            반갑습니다. 요리하다가 개발하게 된 조우식입니다.
           </h1>
-          <p className="text-lg">
-            아래의 버튼을 눌러서 원하는 서비스로 이동하실 수 있습니다.
-          </p>
         </div>
       </header>
-
-      <main className="flex-grow"></main>
-
+      <main className="flex-grow ">
+        <StackElement />
+      </main>
       <footer>
         <ProjectElement />
       </footer>
