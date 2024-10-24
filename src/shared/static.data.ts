e648e6b2318a806092fcadd86afdbaa1a252c25b
조우식 <chousik01@gmail.com>
2024-10-24 18:48:55 +0900

    feata | static.data.ts
    정적 데이터 순서 변경.

--- src/shared/static.data.ts
+++ src/shared/static.data.ts
@@ -9,8 +9,61 @@ export const staticData: IstaticData[] = [
     img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/GoodStart/goodStart_SampleIMG.webp",
     details: {
       introduce:
+        "웹 개발을 처음 배워보면서 가장 기억에 남는 사이드 프로젝트입니다.",
+      params:
+        "웹 페이지 라고 부르기에 모자람이 많은 작업물입니다만, 이 페이지를 통해 모듈화 구조, 외부에서 데이터를 불러올 수 있다는 개념, 이벤트의 버블링과 캡쳐링 등 많은 것을 깨우치게 된 프로젝트입니다.",
+      about: [
+        {
+          placeholder: "notion",
+          link: "https://delicious-corn-899.notion.site/129b1f89bb83816db18ee2a766eac8ce",
+        },
+        {
+          placeholder: "Github",
+          link: "https://github.com/Saccharin01/Good-Start",
+        },
+        {
+          placeholder: "Github Pages",
+          link: "https://saccharin01.github.io/Good-Start/",
+        },
+      ],
+    },
+  },
+  {
+    title: "LoA Music",
+    description:
+      "팀 프로젝트를 진행하며 학습한 내용을 이용해서 스스로의 도메인으로 배포까지 해보는 개인 프로젝트",
+    stack: ["next", "mongoDB Atlas", "AWS S3", "AWS Amplify"],
+    img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/LoA_Music/loaMusic_index.webp",
+    details: {
+      introduce:
         "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
-      params : "",
+      params: "",
+      about: [
+        {
+          placeholder: "notion",
+          link: "https://delicious-corn-899.notion.site/LoA-Music-123b1f89bb83810f9d9fd6506551536a",
+        },
+        {
+          placeholder: "GitHub",
+          link: "https://github.com/Saccharin01/LoA_Music",
+        },
+        {
+          placeholder: "Deploy Link",
+          link: "https://www.saccharin01.com/music",
+        },
+      ],
+    },
+  },
+  {
+    title: "Hackerton Challenge",
+    description:
+      "KDT 해커톤 참여를 위해 자료 조사 및 서류 작업 준비, 그리고 참여 신청까지 했지만 아쉽게도 본선에 진입하지 못한 프로젝트",
+    stack: ["notion"],
+    img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/GoodStart/goodStart_SampleIMG.webp",
+    details: {
+      introduce:
+        "학원 수강 기간 중 해커톤이 개최된다는 소식을 듣고 직접 참여 해보기 위해 팀을 구성하고, 여러 방면으로 자료 조사 및 참여를 위해 준비한 기획입니다. \n안타깝게도 해당 해커톤은 본선 진출이 무산되었지만 평소 잘 모르던 도메인에 대한 자료 조사와 팀간 의견 조율을 진행했던 과정입니다.",
+      params: "",
       about: [
         {
           placeholder: "notion",
@@ -28,8 +81,8 @@ export const staticData: IstaticData[] = [
     details: {
       introduce:
         "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
-        params : "",
-        about: [
+      params: "",
+      about: [
         {
           placeholder: "notion",
           link: "",
@@ -40,43 +93,17 @@ export const staticData: IstaticData[] = [
   {
     title: "Taison Tiger",
     description:
-      "HTML, CSS, JS를 핸들링 해보며 이벤트를 익히기 위해 작성한 html, js",
-    stack: ["HTML", "CSS", "JavaScript"],
+      "머신러닝 기술을 이용해서 모델을 학습시키고, 학습된 모델에서 나온 예측 값을 활용해 웹 페이지를 구성하는 팀 프로젝트",
+    stack: ["Next.js", "FastAPI", "Pytorch", "Tensorflow"],
     img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/taison/taison_index.webp",
     details: {
       introduce:
         "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
-        params : "",
-        about: [
+      params: "",
+      about: [
         {
           placeholder: "notion",
           link: "",
-        },
-      ],
-    },
-  },
-  {
-    title: "LoA Music",
-    description:
-      "팀 프로젝트를 진행하며 학습한 내용을 이용해서 스스로의 도메인으로 만드는 개인 프로젝트",
-    stack: ['next','mongoDB Atlas',"AWS S3", "AWS Amplify"],
-    img: "https://saccharinportfolio.s3.ap-northeast-2.amazonaws.com/LoA_Music/loaMusic_index.webp",
-    details: {
-      introduce:
-        "이 프로젝트에서는 어쩌고 저쩌고를 한 모시깽이 저시깽이 입니다!",
-        params : "",
-        about: [
-        {
-          placeholder: "notion",
-          link: "https://delicious-corn-899.notion.site/LoA-Music-123b1f89bb83810f9d9fd6506551536a",
-        },
-        {
-          placeholder: "GitHub",
-          link: "https://github.com/Saccharin01/LoA_Music",
-        },
-        {
-          placeholder: "Deploy",
-          link: "https://www.saccharin01.com/music",
         },
       ],
     },
