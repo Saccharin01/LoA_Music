cd5e73c6cb04e32e8504db5dfbc7e87c3697adf9
조우식 <chousik01@gmail.com>
2024-10-25 20:02:02 +0900

    style | ProjectElement.tsx

--- src/app/components/ProjectElement.tsx
+++ src/app/components/ProjectElement.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import StackTags from "./StackTags";
 
-
 export default function ProjectElement() {
   const { setLiteral } = useLiteral();
 
@@ -17,35 +16,41 @@ export default function ProjectElement() {
   return (
     <div>
       <div>
-      <h1 className="text-2xl font-bold mb-6">Projects List</h1>
+        <h1 className="text-2xl font-bold mb-6">Projects List</h1>
       </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {staticData.map((element, index) => (
-          <div
-            key={index}
-            className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow"
-            onClick={() => handleSelectData(element)}
-          >
-            <Link href="/details">
-              <div className="flex justify-center items-center">
+          <Link href="/details" key={index}>
+            <div
+              className="rounded-lg shadow-md hover:shadow-lg transition-shadow h-[500px] flex flex-col shadow-gray-700 hover:shadow-gray-700 justify-around"
+              onClick={() => handleSelectData(element)}
+            >
+              {/* 외부 div로 감싸서 구조 변경 */}
+              <div className="h-1/2 flex justify-center items-center overflow-hidden rounded-t-lg">
+                {/* 이미지 */}
                 <Image
                   src={element.img}
                   alt="이미지입니다!"
-                  width={300} // 원래 설정된 너비
-                  height={400} // 원래 설정된 높이
-                  className="w-auto h-auto" // Tailwind로 너비와 높이를 자동으로 설정
+                  width={300}
+                  height={200} // 고정 높이 설정
+                  className="object-cover w-full h-full"
                   priority
                 />
               </div>
 
-              <div>
-                <h2 className="text-xl font-semibold mb-2">{element.title}</h2>
-                <p className="text-gray-700 mb-2">{element.description}</p>
-                {/* <p className="text-gray-500">{element.details.introduce}</p> */}
+              {/* params */}
+              <div className="h-1/4 px-4 py-2">
+                <h2 className="text-xl font-semibold mb-1">{element.title}</h2>
+                <p className="text-gray-700">{element.description}</p>
               </div>
-              <StackTags stackList={element.stack}/>
-            </Link>
-          </div>
+
+              {/* tagList */}
+              <div className="h-1/4 px-4 py-2">
+                <StackTags stackList={element.stack} />
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
