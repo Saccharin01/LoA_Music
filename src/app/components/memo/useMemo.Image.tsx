5e235a5e89c00e5351bfebed8f503b96a44bace0
Saccharin01 <chousik01@gmail.com>
2024-10-29 02:00:41 +0900

    style | useMemo.Image.tsx

--- src/app/components/memo/useMemo.Image.tsx
+++ src/app/components/memo/useMemo.Image.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Image from "next/image";
-import {ImageItemProps} from "../../../shared/interface/useMemo.interface"
+import { ImageItemProps } from "../../../shared/interface/useMemo.interface";
 
 const ImageItem: React.FC<ImageItemProps> = ({ element, handleDragStart }) => {
   return (
     <div
-      className="flex justify-center items-center m-2 h-40 cursor-pointer w-80"
+      className="flex justify-center items-center m-2 h-48 cursor-pointer w-80 rounded-md overflow-hidden transition-transform duration-200 hover:scale-105"
       draggable
       onDragStart={(event) => handleDragStart(event, element)}
     >
       <Image
         src={element.img}
         alt={element._id}
-        className="h-full"
+        className="object-cover"
         width={300}
-        height={160}
+        height={200}
         loading="lazy"
       />
     </div>
