161ff1300295af35329fd013a9072560c9f38ffc
Saccharin01 <chousik01@gmail.com>
2024-10-29 02:00:22 +0900

    style | page.tsx / player

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,25 +1,27 @@
 import DragElement from "../components/DragElement";
 import DropBox from "../components/DropBox";
 import MusicPlayer from "../components/MusicPlayer";
-import Description from "../components/Description"
+import Description from "../components/Description";
 
-export default function MsuicPage() {
+export default function MusicPage() {
   return (
-    <div className="w-[99vw] h-[95vh] relative">
-      <div className="flex h-[60%] justify-between mt-16 ">
-        <main className="w-3/5 flex justify-center items-center">
+    <div className="w-full min-h-screen relative p-4 sm:p-8 flex flex-col space-y-4">
+      {/* 상단 섹션 */}
+      <div className="flex flex-col lg:flex-row flex-grow justify-between mt-8 space-y-4 lg:space-y-0 lg:space-x-4 h-1/2 gap-10">
+        <main className="w-full lg:w-3/5 flex justify-center items-center">
           <MusicPlayer />
         </main>
-        <aside className="flex w-2/5 my-5 flex-col justify-between">
-          <div className="bg-[#9e9e9e] h-4/6">
-          <Description/>
+        <aside className="flex w-full lg:w-2/5 flex-col justify-between space-y-4 lg:space-y-0">
+          <div className="bg-[#9e9e9e] min-h-[200px] sm:min-h-[300px] flex-grow h-1/2">
+            <Description />
           </div>
-          <div className="h-56 flex justify-center items-center">
+          <div className="min-h-[100px] sm:min-h-[150px] flex justify-center items-center flex-grow h-1/2">
             <DropBox />
           </div>
         </aside>
       </div>
-      <footer className=" h-[40%] ml-5">
+      {/* 하단 섹션 */}
+      <footer className="flex-grow h-1/2 mt-4 lg:mt-0 min-h-[150px]">
         <DragElement />
       </footer>
     </div>
