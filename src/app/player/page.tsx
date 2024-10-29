d12cb7e20b7e11082ce1e00ca694215f26fd33fd
Saccharin01 <chousik01@gmail.com>
2024-10-29 13:06:44 +0900

    feat | page.tsx
    페이지 ref 값 부여 및 해당 값을 DragElement에 매개변수로 전달.
    ref 값은 스크롤 바의 위치를 관측합니다.

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,28 +1,36 @@
+"use client"
+
 import DragElement from "../components/DragElement";
 import DropBox from "../components/DropBox";
 import MusicPlayer from "../components/MusicPlayer";
 import Description from "../components/Description";
+import React, { useRef } from "react";
 
 export default function MusicPage() {
+  const footerRef = useRef<HTMLDivElement>(null); // footer에 대한 ref 설정
+
   return (
-    <div className="w-full min-h-screen relative p-4 sm:p-8 flex flex-col space-y-4">
+    <div className="w-full px-4 sm:px-8 md:px-12 min-h-[90vh] sm:flex flex-col space-y-4">
       {/* 상단 섹션 */}
-      <div className="flex flex-col lg:flex-row flex-grow justify-between mt-8 space-y-4 lg:space-y-0 lg:space-x-4 h-1/2 gap-10">
-        <main className="w-full lg:w-3/5 flex justify-center items-center">
+      <div className="flex flex-col lg:flex-row justify-between space-y-4 lg:space-y-0 lg:space-x-4 h-full gap-10">
+        <main className="w-full h-full lg:w-3/5 flex justify-center items-center">
           <MusicPlayer />
         </main>
         <aside className="flex w-full lg:w-2/5 flex-col justify-between space-y-4 lg:space-y-0">
-          <div className="bg-[#9e9e9e] min-h-[200px] sm:min-h-[300px] flex-grow h-1/2">
+          <div className="bg-[#9e9e9e] min-h-[200px] sm:min-h-[300px] h-auto">
             <Description />
           </div>
-          <div className="min-h-[100px] sm:min-h-[150px] flex justify-center items-center flex-grow h-1/2">
+          <div className="flex justify-center items-center">
             <DropBox />
           </div>
         </aside>
       </div>
       {/* 하단 섹션 */}
-      <footer className="flex-grow h-1/2 mt-4 lg:mt-0 min-h-[150px]">
-        <DragElement />
+      <footer
+        ref={footerRef} // 여기서 footerRef 추가
+        className="h-[40vh] mt-4 lg:mt-0 overflow-y-scroll hide-scrollbar"
+      >
+        <DragElement footerRef={footerRef} />
       </footer>
     </div>
   );
