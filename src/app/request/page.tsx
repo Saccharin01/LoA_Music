03e7a9014621d08e55edd0717a5592adc0793a3a
조우식 <chousik01@gmail.com>
2024-10-29 19:32:36 +0900

    feat | page.tsx / request
    요청사항 페이지 레이아웃 수정
    키 다운 이벤트 도큐먼트 레벨로 격상 및 키 조합으로 전송될 수 있게 수정
    입력 값 ref 추가

--- src/app/request/page.tsx
+++ src/app/request/page.tsx
@@ -1,46 +1,60 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Validate from "./modules/Validate";
 
 export default function RequestPage() {
   const requestLogRef = useRef<HTMLInputElement>(null);
+  const requestBodyRef = useRef<HTMLTextAreaElement>(null);
 
-  
   const handleSubmit = () => {
-    Validate(requestLogRef, "/api/request");
+    Validate(
+      { logRef: requestLogRef, bodyRef: requestBodyRef },
+      "/api/request"
+    );
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      handleSubmit(); 
-    }
-  };
+  useEffect(() => {
+    const handleKeyCombination = (event: KeyboardEvent) => {
+      if (event.key === "Enter" && event.ctrlKey) {
+        handleSubmit();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyCombination);
+    return () => {
+      document.removeEventListener("keydown", handleKeyCombination);
+    };
+  }, []);
 
   return (
-    <div className="w-full h-full">
-      <div className="p-40">
-        <div className="bg-[#9e9e9e] bg-opacity-35">
-        <div>
+    <div className="w-full h-full flex justify-center items-center p-4 sm:p-10">
+      <div className="flex flex-col h-full p-4 sm:p-10 bg-[#9e9e9e] bg-opacity-35 rounded-lg shadow-lg w-full max-w-lg sm:max-w-xl lg:max-w-3xl">
+        <div className="mb-4">
           <input
             ref={requestLogRef}
             name="requestLog"
             placeholder="요청사항입니다"
             type="text"
-            onKeyDown={handleKeyDown}
-            className="w-[29rem]"
+            className="w-full p-2 border border-gray-300 rounded-md"
           />
         </div>
-        <div>
+        <div className="mb-4">
+          <textarea
+            ref={requestBodyRef}
+            name="requestBody"
+            placeholder="불편한 부분이 있다면 적어주세요. ctrl+enter 키로 전송할 수도 있습니다!"
+            className="w-full p-2 border border-gray-300 rounded-md resize-none aspect-[3/1]"
+          />
+        </div>
+        <div className="flex justify-center">
           <button
             onClick={handleSubmit}
-            className="border-2 border-solid border-gray-500 hover:border-white hover:text-white mt-20"
+            className="px-4 py-2 border-2 border-solid border-gray-500 hover:border-white hover:text-white rounded-md"
           >
             요청 전송
           </button>
         </div>
-
-        </div>
       </div>
     </div>
   );
-}  
+}
