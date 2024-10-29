2044d6e3bff0575eeecd0713d398f53bcc73735f
조우식 <chousik01@gmail.com>
2024-10-29 18:18:24 +0900

    anno | DragElement.tsx

--- src/app/components/DragElement.tsx
+++ src/app/components/DragElement.tsx
@@ -12,7 +12,9 @@ interface DragElementProps {
 
 export default function DragElement({ footerRef }: DragElementProps) {
   const { data, loading } = useData();
-  const [visibleCount, setVisibleCount] = useState(10); // 표시할 데이터 개수
+
+  // * 페이징 관련 상태 값. 10개씩 불러오고 footerRef 매개변수를 통해 전달되는 props를 이용해 관측합니다.
+  const [visibleCount, setVisibleCount] = useState(10);
 
   const handleDragStart = useCallback(
     (event: React.DragEvent<HTMLDivElement>, item: IMusicDataFormat) => {
@@ -22,7 +24,6 @@ export default function DragElement({ footerRef }: DragElementProps) {
     []
   );
 
-  // footer 내 스크롤 이벤트 핸들러
   const handleScroll = useCallback(() => {
     if (footerRef.current) {
       const { scrollTop, scrollHeight, clientHeight } = footerRef.current;
@@ -46,6 +47,10 @@ export default function DragElement({ footerRef }: DragElementProps) {
     }
   }, [handleScroll, footerRef, loading]);
 
+  /**
+   * * 각 이미지가 리랜더링 되는 부분을 방지하기 위한 Memo 함수입니다
+   * * 의존성 배열로 정의되어있는 data, VisibleCount 등의 값이 변경되지 않는 한 리랜더링을 진행하지 않습니다
+   */
   const imageList = useMemo(() => {
     if (loading) return null;
     return data.slice(0, visibleCount).map((element, index) => (
@@ -58,7 +63,7 @@ export default function DragElement({ footerRef }: DragElementProps) {
   }, [data, visibleCount, handleDragStart, loading]);
 
   if (loading) {
-    return null; // 로딩 중일 때는 빈 화면을 반환
+    return null;
   }
 
   return (
