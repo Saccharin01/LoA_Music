81d05883ff67e8431b1501c6960f34a3e95ad2c8
조우식 <chousik01@gmail.com>
2024-10-29 18:18:59 +0900

    style | useMemo.Image.tsx

--- src/app/components/hooks/memo/useMemo.Image.tsx
+++ src/app/components/hooks/memo/useMemo.Image.tsx
@@ -12,11 +12,11 @@ const ImageItem: React.FC<ImageItemProps> = ({ element, handleDragStart }) => {
       <Image
         src={element.img}
         alt={element._id}
-        className="object-cover w-full h-full" // 부모 요소에 맞게 가득 채우기
+        className="object-cover w-full h-full"
         width={300}
         height={200}
         sizes="(max-width: 600px) 100vw, (max-width: 768px) 50vw, 25vw"
-        priority // 뷰포트에 따라 크기 조정
+        priority 
       />
     </div>
   );
