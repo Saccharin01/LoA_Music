ef2dc1336fe2ab6ef4e5a24385a9ec78ad7721f5
Saccharin01 <chousik01@gmail.com>
2024-10-31 05:12:15 +0900

    style | page.tsx
    스타일 조정

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -10,14 +10,14 @@ export default function MusicPage() {
   const footerRef = useRef<HTMLDivElement>(null); // footer에 대한 ref 설정
 
   return (
-    <div className="w-full px-4 sm:px-8 md:px-12 min-h-[90vh] sm:flex flex-col space-y-4">
+    <div className="w-full px-4 sm:px-8 md:px-12 min-h-[90vh] sm:flex flex-col space-y-4 justify-between">
       {/* 상단 섹션 */}
       <div className="flex flex-col lg:flex-row justify-between space-y-4 lg:space-y-0 lg:space-x-4 h-full gap-10">
         <main className="w-full h-full lg:w-3/5 flex justify-center items-center">
           <MusicPlayer />
         </main>
         <aside className="flex w-full lg:w-2/5 flex-col justify-between space-y-4 lg:space-y-0">
-          <div className="bg-[#9e9e9e] min-h-[200px] sm:min-h-[300px] h-auto">
+          <div className="bg-[#9e9e9e] min-h-[200px] sm:min-h-[10rem] h-auto rounded-lg">
             <Description />
           </div>
           <div className="flex justify-center items-center">
