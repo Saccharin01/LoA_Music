67260602b23d42c001e488afbdc7e5f4e0ecbab7
Ssacahrin01 <thacalin@Saccharin01.local>
2024-10-30 16:04:46 +0900

    feat | useMemo.interface.ts
    메모제이션 이미지 컴포넌트의 인터페이스 수정
    클릭 이벤트를 정의하였습니다

--- src/shared/interface/useMemo.interface.ts
+++ src/shared/interface/useMemo.interface.ts
@@ -1,6 +1,7 @@
 import { IMusicDataFormat } from "@/shared/IDataFormat";
 
 export interface ImageItemProps {
-  element: IMusicDataFormat;
-  handleDragStart: (event: React.DragEvent<HTMLDivElement>, item: IMusicDataFormat) => void;
-}
+    element: IMusicDataFormat;
+    handleDragStart: (event: React.DragEvent<HTMLDivElement>, element: IMusicDataFormat) => void;
+    onClick: () => void; // onClick 속성을 선택적으로 추가
+  }
