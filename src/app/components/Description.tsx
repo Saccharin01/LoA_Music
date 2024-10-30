c897d45beaaa49e204c4ee3a9e79698fda749335
Saccharin01 <chousik01@gmail.com>
2024-10-31 05:09:11 +0900

    style | Description.tsx
    설명 컴포넌트의 레이아웃 수정 및 CSS 조정

--- src/app/components/Description.tsx
+++ src/app/components/Description.tsx
@@ -7,13 +7,14 @@ export default function MusicDescription() {
   const { droppedItem } = useDragDrop();
   if (!droppedItem || !droppedItem.description) {
     return (
-      <div className="grid grid-cols-1 gap-4 p-4 rounded-lg shadow-md h-[90%] mt-[px]">
-        <div className="bg-orange-100 p-4 rounded-lg shadow-md">
-          <h2 className="text-xl font-bold mb-2">불러온 내용이 없어요</h2>
-          <p className="text-sm text-gray-700">아래의 버튼을 이용해서 장르를 선택 해주세요</p>
-        </div>
-        <div className="bg-gray-100 p-4 rounded-lg shadow-md flex justify-center items-center">
+      <div className="grid grid-cols-1 gap-4 p-4 rounded-lg h-full justify-between">
+        <div className="bg-gray-100 p-4 rounded-lg flex justify-center items-center">
           <GenreNav />
+        </div>
+
+        <div className="bg-gray-100 p-4 rounded-lg">
+          <h2 className="text-xl font-bold mb-2">불러온 내용이 없어요</h2>
+          <p className="text-lg text-gray-700">위 버튼을 클릭하면 노래를 불러올 수 있어요!</p>
         </div>
       </div>
     );
@@ -22,17 +23,17 @@ export default function MusicDescription() {
   const { headLine, mainScript } = droppedItem.description;
 
   return (
-    <div className="grid grid-cols-1 gap-4 p-4 rounded-lg shadow-md h-[90%] mt-[px]">
+    <div className="grid grid-cols-1 gap-4 p-4 rounded-lg h-full">
+      <div className="bg-gray-100 p-4 rounded-lg flex justify-center items-center">
+        <GenreNav />
+      </div>
       <div
-        className={`bg-orange-100 p-4 rounded-lg shadow-md ${
+        className={`bg-gray-100 p-4 rounded-lg shadow-md ${
           headLine && mainScript ? "block" : "hidden"
         }`}
       >
         <h2 className="text-xl font-bold mb-2">{headLine}</h2>
         <p className="text-sm text-gray-700">{mainScript}</p>
-      </div>
-      <div className="bg-gray-100 p-4 rounded-lg shadow-md flex justify-center items-center">
-        <GenreNav />
       </div>
     </div>
   );
